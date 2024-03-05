c2d519df060da9af2acd32ad05bce66ca5bc5e3f
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2024-03-06 10:58:54 +1100

    Set tabstop to 4

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -19,8 +19,8 @@ opt.cursorline = true
 opt.expandtab = true
 opt.shiftwidth = 2
 opt.smartindent = true
-opt.tabstop = 2
-opt.softtabstop = 2
+opt.tabstop = 4
+opt.softtabstop = 4
 
 opt.fillchars = { eob = " " }
 opt.ignorecase = true
