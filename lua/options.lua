3191d9b44f6f204d9f1f2c9aa89259793e1c8c53
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2024-07-07 14:20:33 +1000

    Update vim mapping

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+
+o.cursorlineopt = "both"
+o.scrolloff = 10
