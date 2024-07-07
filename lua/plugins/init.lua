b51846dd2fc1ecaa2658e9b5bd2fed6c01d67fb9
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2024-07-07 14:37:03 +1000

    Add Ollama

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -57,4 +57,15 @@ return {
       signs = false,
     },
   },
+  {
+    "David-Kunz/gen.nvim",
+    ft = "go",
+    config = function()
+      require("gen").setup {
+        model = "llama2-uncensored",
+        host = "localhost",
+        port = "11434",
+      }
+    end,
+  },
 }
