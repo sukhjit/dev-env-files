910d392dcad01711c5da79a3f650ec8e7bc878da
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2024-07-08 20:23:53 +1000

    Telescope plugin

--- init.lua
+++ init.lua
@@ -108,24 +108,11 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require("lazy").setup({ import = "custom/plugins" }, {
   change_detection = {
     notify = false,
   },
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
       cmd = "⌘",
       config = "🛠",
