9a4f0f5d6f86da4fbcb7e3a3c33f5a64808ebccc
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2024-07-08 16:56:01 +1000

    blink on yank

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,11 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
