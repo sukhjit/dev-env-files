3c8cda062d4489a4acac822dad2bfb0ddee20824
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2024-07-08 14:48:27 +1000

    go tests

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -44,6 +44,7 @@ return {
         "css",
         "hcl",
         "terraform",
+        "go",
       },
     },
   },
@@ -71,5 +72,78 @@ return {
   {
     "tpope/vim-fugitive",
     event = "VeryLazy",
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "fredrikaverpil/neotest-golang",
+    },
+    keys = {
+      {
+        "<leader>tt",
+        function()
+          require("neotest").run.run(vim.fn.expand "%")
+        end,
+        desc = "Run File",
+      },
+      {
+        "<leader>tT",
+        function()
+          require("neotest").run.run(vim.loop.cwd())
+        end,
+        desc = "Run All Test Files",
+      },
+      {
+        "<leader>tr",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "Run Nearest",
+      },
+      {
+        "<leader>ts",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "Toggle Summary",
+      },
+      {
+        "<leader>to",
+        function()
+          require("neotest").output.open { enter = true, auto_close = true }
+        end,
+        desc = "Show Output",
+      },
+      {
+        "<leader>tO",
+        function()
+          require("neotest").output_panel.toggle()
+        end,
+        desc = "Toggle Output Panel",
+      },
+      {
+        "<leader>tS",
+        function()
+          require("neotest").run.stop()
+        end,
+        desc = "Stop",
+      },
+    },
+    config = function()
+      require("neotest").setup {
+        adapters = {
+          require "neotest-golang" {
+            go_test_args = {
+              "-v",
+              "-race",
+            },
+          },
+        },
+      }
+    end,
   },
 }
