84d40a152a0dc0233e7ca52adb10144eaa360c2b
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2024-07-08 21:22:37 +1000

    tab setting

--- init.lua
+++ init.lua
@@ -3,8 +3,10 @@ vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
--- Make line numbers default
 vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
