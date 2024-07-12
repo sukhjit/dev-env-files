2149d014c628751289087f970de9b5660afe9405
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2024-07-12 10:11:07 +1000

    highlight text on yank

--- lua/mine/core/keymaps.lua
+++ lua/mine/core/keymaps.lua
@@ -2,6 +2,15 @@ vim.g.mapleader = " "
 
 vim.g.have_nerd_font = true
 
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 local set = vim.keymap.set
 
 set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
