bdd86193ce6de93d25c68d5f40193f5c647a7978
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2024-07-16 09:55:44 +1000

    show hidden files in neotree

--- nvim/lua/mine/plugins/neotree.lua
+++ nvim/lua/mine/plugins/neotree.lua
@@ -18,6 +18,19 @@ return {
             ["\\"] = "close_window",
           },
         },
+        filtered_items = {
+          visible = false,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".DS_Store",
+            "thumbs.db",
+          },
+          never_show = {
+            ".git",
+          },
+        },
       },
     },
   },
