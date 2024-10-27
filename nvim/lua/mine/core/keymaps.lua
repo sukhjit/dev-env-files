aab784956b8595ef222ba538482ae3cec04be718
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2024-10-28 09:50:05 +1100

    Add llama.cpp config for Auto completion

--- nvim/lua/mine/core/keymaps.lua
+++ nvim/lua/mine/core/keymaps.lua
@@ -35,3 +35,5 @@ set("n", "<M-s>", "<C-W>-")
 
 set("v", "K", ":m '<-2<CR>gv-gv")
 set("v", "J", ":m '>+1<CR>gv-gv")
+
+set("n", "<leader>ll", "<cmd>call llama#init()<CR>", { desc = "Start llama AI" })
