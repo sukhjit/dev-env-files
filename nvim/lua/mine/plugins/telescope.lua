d44eab94166938654a270ef9a52f2624ca58bb31
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2024-12-03 15:12:15 +1100

    Telescope shortcuts

--- nvim/lua/mine/plugins/telescope.lua
+++ nvim/lua/mine/plugins/telescope.lua
@@ -21,6 +21,30 @@ return {
     },
     config = function()
       require("telescope").setup {
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
+              ["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
+              ["<C-l>"] = require("telescope.actions").select_default, -- open file
+            },
+          },
+        },
+
+        pickers = {
+          find_files = {
+            file_ignore_patterns = { "node_modules", ".git", ".venv" },
+            hidden = true,
+          },
+        },
+
+        live_grep = {
+          file_ignore_patterns = { "node_modules", ".git", ".venv" },
+          additional_args = function(_)
+            return { "--hidden" }
+          end,
+        },
+
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown(),
