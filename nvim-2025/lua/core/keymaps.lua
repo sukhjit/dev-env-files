b2b812235c68baada6503fe1fe8aa52c621e5ccc
Sam Singh <cassini2@gmail.com>
2025-08-22 13:47:54 +1000

    Remove keymaps for diagnostic jump

--- nvim-2025/lua/core/keymaps.lua
+++ nvim-2025/lua/core/keymaps.lua
@@ -12,8 +12,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- move text after cursor to next line
 vim.keymap.set('n', '<leader>j', 'i<CR><Esc>')
 
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
