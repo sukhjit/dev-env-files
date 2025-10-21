d78919da9113d50c7cecff7b9011fe4f75fb1d56
Sam Singh <cassini2@gmail.com>
2025-10-22 09:04:15 +1100

    Wofi improvements

---
+++ Deepcool/wofi/scripts/wafinder.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+menu=" Calculator\n Cancel"
+
+selected=$(echo -e $menu | wofi -W 10% --dmenu --line 4 --cache-file /dev/null -p "Apps" | awk '{print tolower ($2)}')
+
+calculator () {
+    notify-send $(wofi -W 30% --dmenu --line 1  -p "calc" | bc -l 2>/dev/null)
+}
+
+case $selected in
+   calculator)
+      calculator;;
+   cancel)
+      exit 0;;
+esac
