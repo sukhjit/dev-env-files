b053400d6fac5df4b44fd65b564048eff747c5cc
Sam Singh <cassini2@gmail.com>
2025-10-22 12:54:22 +1100

    Wofi note taking

---
+++ Deepcool/wofi/scripts/notes.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+folder="$HOME/Dropbox/Documents/Notes/"
+TERMINAL="ghostty"
+woficmd="wofi --dmenu -k /dev/null"
+
+newnote () { \
+  dir=$folder
+  name="$(echo "" | $woficmd -p "Enter a name")" || exit 0
+  : "${name:=$(date +%F_%H-%M-%S)}"
+  setsid -f "$TERMINAL" -e nvim $dir$name".md" >/dev/null 2>&1
+}
+
+selected () { \
+  curfiles=$(find $folder -type f -printf '%T@ %P\n' | sort -nr | cut -d' ' -f2-)
+  choice=$(
+    echo -e "New\n$curfiles" | $woficmd -c -l 5 -i -p "Choose note or create new: "
+  )
+  case $choice in
+    New) newnote ;;
+    *.md) setsid -f "$TERMINAL" -e nvim "$folder$choice" >/dev/null 2>&1 ;;
+    *) exit ;;
+  esac
+}
+
+selected
--- Deepcool/wofi/scripts/wafinder.sh
+++ Deepcool/wofi/scripts/wafinder.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-menu=" Calculator\n Cancel"
+menu=" Calculator\n󰎚 Note\n Cancel"
 
 selected=$(echo -e $menu | wofi -W 10% --dmenu --line 4 --cache-file /dev/null -p "Apps" | awk '{print tolower ($2)}')
 
@@ -8,9 +8,15 @@ calculator () {
     notify-send $(wofi -W 10% --dmenu --line 1  -p "calc" | bc -l 2>/dev/null)
 }
 
+note () {
+    source ~/.config/wofi/scripts/notes.sh
+}
+
 case $selected in
    calculator)
       calculator;;
+   note)
+      note;;
    cancel)
       exit 0;;
 esac
