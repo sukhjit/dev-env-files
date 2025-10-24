20454e5c182da80c417c075aaa3a1e72baff276d
Sam Singh <cassini2@gmail.com>
2025-10-24 16:19:07 +1100

    Update audio switcher

--- Deepcool/wofi/scripts/audioswitch.sh
+++ Deepcool/wofi/scripts/audioswitch.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-# Create a list of sinks with pretty names
-options=$(pactl -f json list sinks | jq -r '.[] | .description')
+headphones() {
+    pactl set-default-sink "alsa_output.pci-0000_10_00.6.analog-stereo" &&
+        notify-send "Audio switched to headphones"
+}
 
-# Let the user select a description
-selection=$(echo "$options" | wofi --dmenu --cache-file /dev/null -W 30% -i -L 4 -c -p "Output:")
+speakers() {
+    pactl set-default-sink "alsa_output.pci-0000_01_00.1.hdmi-stereo" &&
+        notify-send "Audio switched to speakers"
+}
 
-# Extract the corresponding sink name
-sink_name=$(pactl -f json list sinks | jq -r --arg sink_pretty_name "$selection" '.[] | select(.description == $sink_pretty_name) | .name')
+choosespeakers() {
+    options="Headphones\nSpeakers"
+    choice=$(echo -e "$options" | wofi --dmenu --cache-file /dev/null -W 10% -i -L 3 -p "Choose audio:")
 
-# Set the selected sink as default
-if [ -n "$sink_name" ]; then
-    pactl set-default-sink "$sink_name" && notify-send "Audio switched to: $selection"
-else
-    notify-send "Audio switch failed"
-fi
+    case "$choice" in
+    Speakers) speakers ;;
+    Headphones) headphones ;;
+    esac
+}
+
+choosespeakers
