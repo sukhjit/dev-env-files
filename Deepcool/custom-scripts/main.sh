f420a2bd2aaddda2d30bd11d7e13f95e62e6eda5
Sam Singh <cassini2@gmail.com>
2025-10-25 14:13:33 +1100

    Keybindings list

--- Deepcool/custom-scripts/main.sh
+++ Deepcool/custom-scripts/main.sh
@@ -4,10 +4,6 @@ calculator() {
     notify-send $(wofi -W 10% --dmenu --line 1 -p "calc" | bc -l 2>/dev/null)
 }
 
-note() {
-    source $XDG_CONFIG_HOME/custom-scripts/notes.sh
-}
-
 screenshot() {
     hyprshot -m region --raw |
         satty --filename - \
@@ -18,28 +14,35 @@ screenshot() {
             --copy-command 'wl-copy'
 }
 
-battery() {
-    source $XDG_CONFIG_HOME/custom-scripts/battery.sh
-}
-
-audioswitch() {
-    source $XDG_CONFIG_HOME/custom-scripts/audioswitch.sh
-}
-
 show_main_menu() {
-    menu="  Audio\n Calculator\n󰎚 Note\n Screenshot\n Battery\n Cancel"
+    menu="  Audio\n Calculator\n Keybindings\n󰎚 Note\n Screenshot\n Battery\n Cancel"
 
     walinecount=$(echo -e $menu | wc -l)
 
     selected=$(echo -e $menu | wofi -W 10% --dmenu --line $walinecount --cache-file /dev/null -p "Apps" | awk '{print tolower ($2)}')
 
     case $selected in
-    audio) audioswitch ;;
-    calculator) calculator ;;
-    note) note ;;
-    screenshot) screenshot ;;
-    battery) battery ;;
-    cancel) exit 0 ;;
+    audio)
+        source $XDG_CONFIG_HOME/custom-scripts/audioswitch.sh
+        ;;
+    calculator)
+        calculator
+        ;;
+    note)
+        source $XDG_CONFIG_HOME/custom-scripts/notes.sh
+        ;;
+    screenshot)
+        screenshot
+        ;;
+    keybindings)
+        source $XDG_CONFIG_HOME/custom-scripts/keybindings.sh
+        ;;
+    battery)
+        source $XDG_CONFIG_HOME/custom-scripts/battery.sh
+        ;;
+    cancel)
+        exit 0
+        ;;
     esac
 }
 
