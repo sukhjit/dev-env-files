9a0903b1e728c5374e1f0941431fe6a1aa0ffc2f
Sam Singh <cassini2@gmail.com>
2026-04-22 16:52:21 +1000

    Fix telescope settings

--- nvim-202604/init.lua
+++ nvim-202604/init.lua
@@ -157,6 +157,29 @@ require('lazy').setup({
     },
     config = function()
       require('telescope').setup {
+        defaults = {
+          mappings = {
+            i = {
+              ['<esc>'] = require('telescope.actions').close,
+              ['<c-enter>'] = 'to_fuzzy_refine',
+              ['<C-k>'] = require('telescope.actions').move_selection_previous, -- move to prev result
+              ['<C-j>'] = require('telescope.actions').move_selection_next, -- move to next result
+              ['<C-l>'] = require('telescope.actions').select_default, -- open file
+            },
+          },
+        },
+        pickers = {
+          find_files = {
+            file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+            hidden = true,
+          },
+        },
+        live_grep = {
+          file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+          additional_args = function(_)
+            return { '--hidden' }
+          end,
+        },
         extensions = {
           ['ui-select'] = { require('telescope.themes').get_dropdown() },
         },
@@ -179,6 +202,17 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sF', function()
+        builtin.find_files {
+          find_command = {
+            'rg',
+            '--files',
+            '--hidden',
+            '-g',
+            '!.git',
+          },
+        }
+      end, { desc = '[S]earch hidden [F]iles' })
 
       -- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
       -- it is better explained there). This allows easily switching between pickers if you prefer using something else!
