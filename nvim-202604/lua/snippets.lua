c48bf7a1bb45a7cfcd0f3538bb63a5e933c5d504
Sam Singh <cassini2@gmail.com>
2026-05-12 18:01:23 +1000

    nvim no auto continue comments

--- nvim-202604/lua/snippets.lua
+++ nvim-202604/lua/snippets.lua
@@ -79,3 +79,11 @@ vim.api.nvim_create_autocmd('FileType', {
 vim.api.nvim_create_autocmd('VimResized', {
   command = 'wincmd =',
 })
+
+-- no auto continue comments on new line
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('no_auto_comment', {}),
+  callback = function()
+    vim.opt_local.formatoptions:remove { 'c', 'r', 'o' }
+  end,
+})
